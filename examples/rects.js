88a57c18e3e48ffc6382ae15f56126164efde803
Tj Holowaychuk <tj@vision-media.ca>
2011-10-27 15:53:08 -0700

    misc

--- examples/rects.js
+++ examples/rects.js
@@ -23,9 +23,9 @@ process.on('SIGWINCH', function(){
 var canvas = new Canvas(size[1], size[0])
   , ctx = canvas.getContext('2d')
   , x = 0
-  , y = 0
+  , dx = 1
   , x2 = 0
-  , y2 = 0;
+  , dx2 = .5;
 
 ctx.hideCursor();
 setInterval(function(){
@@ -33,8 +33,8 @@ setInterval(function(){
   ctx.strokeStyle = 'blue';
   ctx.strokeRect(1, 1, canvas.width - 1, canvas.height - 1);
   ctx.strokeStyle = 'green';
-  ctx.strokeRect(x++, 2, 30, 5);
+  ctx.strokeRect(x += dx, 2, 30, 5);
   ctx.strokeStyle = 'yellow';
-  ctx.fillRect(x2 += .5, 5, 20, 5);
+  ctx.fillRect(x2 += dx2, 5, 20, 5);
   ctx.moveTo(0, 10);
 }, 1000 / 20);
