d965f3e193d147812a74326c2d6093e32712fa01
Tj Holowaychuk <tj@vision-media.ca>
2011-10-27 16:35:30 -0700

    ground

--- examples/platformer.js
+++ examples/platformer.js
@@ -23,15 +23,28 @@ process.on('SIGWINCH', function(){
 
 var canvas = new Canvas(size[1], size[0])
   , ctx = canvas.getContext('2d')
+  , w = 5
+  , h = 3
+  , sx = 0
+  , sy = 0
   , x = 1
-  , y = 1;
+  , y = canvas.height - h;
 
-// ctx.hideCursor();
-// setInterval(function(){
-//   ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   ctx.fillStyle = 'green';
-//   ctx.fillRect(0, 0, 5, 3);
-// }, 1000 / 20);
+ctx.hideCursor();
+setInterval(function(){
+  x += sx;
+  y += sy;
+  sx *= .5;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // player
+  ctx.fillStyle = 'green';
+  ctx.fillRect(x, y, 5, 3);
+
+  // ground
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, canvas.height, canvas.width, 1);
+}, 1000 / 20);
 
 process.stdin.on('keypress', function(char, key){
   switch (key.name) {
@@ -41,8 +54,10 @@ process.stdin.on('keypress', function(char, key){
     case 'up':
       break;
     case 'left':
+      sx = 5;
       break;
     case 'right':
+      sx = -5;
       break;
   }
 }).resume();
