1a54b43f48a7398c89b985913f8413c75f6a98cb
Tj Holowaychuk <tj@vision-media.ca>
2011-10-28 14:37:02 -0700

    Added lame lines.js example

---
+++ examples/lines.js
@@ -0,0 +1,36 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Canvas = require('../')
+  , tty = require('tty')
+  , size = tty.getWindowSize();
+
+process.on('SIGINT', function(){
+  ctx.restore();
+  process.nextTick(function(){
+    process.exit();
+  });
+});
+
+process.on('SIGWINCH', function(){
+  size = tty.getWindowSize();
+  canvas.width = size[1];
+  canvas.height = size[0];
+});
+
+var canvas = new Canvas(size[1], size[0])
+  , ctx = canvas.getContext('2d');
+
+ctx.hideCursor();
+setInterval(function(){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = 'yellow';
+  ctx.beginPath();
+  ctx.lineTo(5, 5);
+  ctx.lineTo(15, 10);
+  ctx.lineTo(25, 5);
+  ctx.lineTo(5, 5);
+  ctx.stroke();
+}, 1000 / 20);
