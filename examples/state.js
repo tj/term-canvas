417092e12ee46ece6ef0b3ad09f5d2e788800e56
TJ Holowaychuk <tj@vision-media.ca>
2011-11-20 15:26:42 -0800

    Added state management

--- examples/state.js
+++ examples/state.js
@@ -10,12 +10,21 @@ var canvas = new Canvas(50, 100)
 
 ctx.fillStyle = 'red';
 ctx.fillRect(5, 5, 20, 10);
+ctx.save();
 
 ctx.fillStyle = 'blue';
 ctx.fillRect(6, 6, 18, 8);
+ctx.save();
 
 ctx.fillStyle = 'yellow';
 ctx.fillRect(7, 7, 16, 6);
 
-console.log('\n\n\n');
+ctx.restore();
+ctx.fillRect(8, 8, 14, 4);
+
+ctx.restore();
+ctx.fillRect(9, 9, 12, 2);
+
+
+console.log('\n\n\n\n\n');
 ctx.resetState();
