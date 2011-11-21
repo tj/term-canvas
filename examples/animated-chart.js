f03afd3052e4af7dad8a12256abae56e6e55c260
TJ Holowaychuk <tj@vision-media.ca>
2011-11-20 16:21:40 -0800

    Added a somewhat broken animated chart example

---
+++ examples/animated-chart.js
@@ -0,0 +1,40 @@
+
+var Canvas = require('../')
+  , canvas = new Canvas(50, 50)
+  , ctx = canvas.getContext('2d');
+
+process.on('SIGINT', function(){
+  ctx.reset();
+  process.nextTick(function(){
+    process.exit();
+  });
+});
+
+Array.prototype.max = function(){
+  return this.sort(function(a, b){
+    return a - b;
+  }).pop();
+};
+
+function rand() {
+  return Math.random() * 5 | 0;
+}
+
+setInterval(function(){
+  var data = [rand(), rand(), rand(), rand(), rand(), rand()]
+    , max = data.max()
+    , x = 3;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  data.forEach(function(n){
+    var y = 20
+      , w = 8
+      , h = 20 * (n / max);
+
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x, y - h + 3, w, h);
+    ctx.fillStyle = 'white';
+    ctx.fillText(n.toString(), x + 3, y + 1);
+    x += 9;
+  });
+}, 1000 / 6);
