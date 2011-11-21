d5453813edb04d5e21cd762638c6312d6937350b
TJ Holowaychuk <tj@vision-media.ca>
2011-11-20 16:03:26 -0800

    Added crappy chart demo

---
+++ examples/chart.js
@@ -0,0 +1,23 @@
+
+var Canvas = require('../')
+  , canvas = new Canvas(50, 50)
+  , ctx = canvas.getContext('2d');
+
+var data = [1, 2, 3, 2, 5, 1]
+  , max = 5
+  , x = 3;
+
+data.forEach(function(n){
+  var y = 20
+    , w = 8
+    , h = 20 * (n / max);
+
+  ctx.fillStyle = 'blue';
+  ctx.fillRect(x, y - h + 3, w, h);
+  ctx.fillStyle = 'white';
+  ctx.fillText(n.toString(), x + 3, y + 1);
+  x += 9;
+});
+
+ctx.resetState();
+console.log('\n\n\n\n\n');
