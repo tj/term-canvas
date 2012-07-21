b73e529f82b2067fd54254804db1aa6eb1627c91
TJ Holowaychuk <tj@vision-media.ca>
2012-07-21 07:00:02 -0700

    add nested translation example

---
+++ examples/nested-translations.js
@@ -0,0 +1,34 @@
+
+/**
+ * Module dependencies.
+ */
+
+var Canvas = require('./')
+  , canvas = new Canvas(100, 100)
+  , ctx = canvas.getContext('2d')
+  , x = 0
+  , y = 0;
+
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+ctx.save();
+ctx.translate(3, 3);
+ctx.fillStyle = 'yellow';
+ctx.fillRect(0, 0, 10, 5);
+
+ctx.save();
+ctx.translate(3, 3);
+ctx.fillStyle = 'red';
+ctx.fillRect(0, 0, 10, 5);
+
+ctx.save();
+ctx.translate(3, 3);
+ctx.fillStyle = 'blue';
+ctx.fillRect(0, 0, 10, 5);
+ctx.restore();
+
+ctx.restore();
+ctx.restore();
+
+console.log('\n\n\n\n');
+ctx.resetState();
